33afafeb1414c611bfa70311c6f323e4f474de0f
blckbx <74455114+blckbx@users.noreply.github.com>
2023-10-31 09:10:44 +0100

    Update bitcoin-whitpaper-extraction.sh

--- bitcoin-whitpaper-extraction.sh
+++ bitcoin-whitpaper-extraction.sh
@@ -1,6 +1,6 @@
 # Extract Bitcoin Whitepaper from Blockchain Data
 # Full Node:
-$ bitcoin-cli getrawtransaction 54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e713  | sed 's/0100000000000000/\n/g' | \
+bitcoin-cli getrawtransaction 54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e713  | sed 's/0100000000000000/\n/g' | \
 tail -n +2 | cut -c7-136,139-268,271-400 | tr -d '\n' | cut -c17-368600 | xxd -p -r > bitcoin.pdf
 
 # Pruned Node:
