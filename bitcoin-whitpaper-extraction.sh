67459acfaed2cc4a868555dceeaf4f6c84e02004
blckbx <74455114+blckbx@users.noreply.github.com>
2023-10-31 09:16:45 +0100

    Update bitcoin-whitpaper-extraction.sh

--- bitcoin-whitpaper-extraction.sh
+++ bitcoin-whitpaper-extraction.sh
@@ -4,6 +4,5 @@ bitcoin-cli getrawtransaction 54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4
 tail -n +2 | cut -c7-136,139-268,271-400 | tr -d '\n' | cut -c17-368600 | xxd -p -r > bitcoin.pdf
 
 # Pruned Node:
-
 seq 0 947 | (while read -r n; do bitcoin-cli gettxout 54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e713 $n | jq -r '.scriptPubKey.asm ' | \
 awk '{ print $2 $3 $4 }'; done) | tr -d '\n' | cut -c 17-368600 | xxd -r -p > bitcoin-pruned.pdf
