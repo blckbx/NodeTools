ad7b9d3d1b7d675e2bae37514323cbf481bb997c
blckbx <74455114+blckbx@users.noreply.github.com>
2023-10-31 10:44:04 +0100

    Update bitcoin-whitepaper-extraction.sh

--- bitcoin-whitepaper-extraction.sh
+++ bitcoin-whitepaper-extraction.sh
@@ -5,4 +5,4 @@ cut -c3- | xxd -p -r | tail +9c | head -c 184292 > bitcoin.pdf
 
 # Pruned Node:
 seq 0 947 | (while read -r n; do bitcoin-cli gettxout 54e48e5f5c656b26c3bca14a8c95aa583d07ebe84dde3b7dd4a78f4e4186e713 $n | jq -r '.scriptPubKey.asm ' | \
-awk '{ print $2 $3 $4 }'; done) | tr -d '\n' | cut -c 17-368600 | xxd -r -p > bitcoin-pruned.pdf
+awk '{ print $2 $3 $4 }'; done) | tr -d '\n' | cut -c 17-368600 | xxd -r -p > bitcoin.pdf
