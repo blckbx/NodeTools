bf40742ab0337735527fc8ebb748e24f5ee97f8f
weasel3 <16792626+weasel3@users.noreply.github.com>
2023-11-29 00:55:53 +0100

    blockheight comparism local to peers

--- healthmonitor.sh
+++ healthmonitor.sh
@@ -6,13 +6,19 @@
 # and long-term CPU usage (15-minute average). 
 # The user is notified via the Telegram bot when any of these 
 # resources surpass predefined thresholds.
+# The script also uses bitcoin-cli to compare the local blockheight 
+# with the majority of connected peers and reports if it differs.
 #
 # made by @fry_aldebaran
 #
 # usage via crontab: */5 * * * * /bin/bash /home/admin/healthmonitor.sh
 #
-# version: 1.0
+# version: 2.0
 # date: 2023-11-28
+
+# define btccli command
+[ -f ~/.bashrc ] && source ~/.bashrc
+[ -z "$_CMD_BTCCLI" ] && _CMD_BTCCLI=/usr/local/bin/bitcoin-cli
 
 # setup telegram bot
 TOKEN="yourtoken"
@@ -24,6 +30,8 @@ swap_threshold=90
 disk_threshold=90
 cpu_threshold=95
 
+# Limit of difference to the majority of your peers' blockheight to report
+blkdiff_limit=1
 
 # push message to TG bot
 pushover() {
@@ -42,6 +50,7 @@ check_resource() {
     local message="$3"
 
     if (( $(awk -v value="$value" -v threshold="$threshold" 'BEGIN { if (value >= threshold) print 1; else print 0; }') )); then
+        echo "WARNING: $message"
         pushover "WARNING: $message"
     fi
 }
@@ -63,3 +72,13 @@ cpu_count=$(nproc)
 load_average=$(top -bn1 | awk '/load average/ {print $(NF)}')
 cpu_usage=$(awk "BEGIN {printf \"%.2f\", 100 / $cpu_count * $load_average}")
 check_resource "$cpu_usage" "$cpu_threshold" "Load Average (15min) at $cpu_usage% ($load_average)!"
+
+# Blockheight compare to peers
+peer_heights=("$($_CMD_BTCCLI getpeerinfo | jq -r '.[] | .synced_blocks' | sort -rn)")
+local_height=$($_CMD_BTCCLI getblockcount)
+echo "majority peers blockheight: ${peer_heights[0]}"
+echo "local blockheight: $local_height"
+
+if (( peer_heights[0] - local_height >= blkdiff_limit )); then
+    pushover "WARNING: local blockheight ($local_height) differs from peers (${peer_heights[0]})!"
+fi
