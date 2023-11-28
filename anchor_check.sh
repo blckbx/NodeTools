9aa122a3f69219c4ef7aa0251332bddd642d432a
HODLmeTight <35168804+TrezorHannes@users.noreply.github.com>
2023-11-28 20:19:50 +0100

    Update anchor_check.sh
    Nice enhancement
    
    Co-authored-by: blckbx <74455114+blckbx@users.noreply.github.com>

--- anchor_check.sh
+++ anchor_check.sh
@@ -33,7 +33,7 @@ while read -r line; do
     PUBKEY=$(echo "$line" | jq -r '.remote_pubkey')
 
     # Check if the pubkey is valid and get the alias name
-    ALIAS=$($anchor_list getnodeinfo --pub_key "$PUBKEY" | jq -r '.node.alias')
+    ALIAS=$($echo "$line" | jq -r '.peer_alias')
     if [ -z "$ALIAS" ]; then
       echo "Invalid node pubkey: $PUBKEY"
       exit 1
