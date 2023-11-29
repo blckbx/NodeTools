472d6c0caaf6cc01ca9fb080e364d5110e630c30
blckbx <74455114+blckbx@users.noreply.github.com>
2023-11-29 12:01:18 +0100

    fast query and getnodeinfo fallback

--- anchor_check.sh
+++ anchor_check.sh
@@ -33,10 +33,13 @@ while read -r line; do
     PUBKEY=$(echo "$line" | jq -r '.remote_pubkey')
 
     # Check if the pubkey is valid and get the alias name
-    ALIAS=$($anchor_list getnodeinfo --pub_key "$PUBKEY" | jq -r '.node.alias')
+    ALIAS=$(echo "$line" | jq -r '.peer_alias')
     if [ -z "$ALIAS" ]; then
-      echo "Invalid node pubkey: $PUBKEY"
-      exit 1
+        ALIAS=$($anchor_list getnodeinfo --pub_key \"$PUBKEY\" | jq -r '.node.alias')
+        if [ -z "$ALIAS" ]; then
+            echo "Invalid node pubkey: $PUBKEY"
+            exit 1
+        fi
     fi
 
     # Calculate the sat_vbyte value
