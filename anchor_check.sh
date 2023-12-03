81adc61e74c407a7a79da0cdbba0fe3ffd7900cd
TrezorHannes <hakezuba@gmail.com>
2023-12-03 20:41:06 +0100

    adjusted --peer to avoid running the loop

--- anchor_check.sh
+++ anchor_check.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-#### GUIDE ###############################################
-# download this script with wget LINKTOFILE
-# assumes lncli is globally available: whereis lncli
-
-#### Execute #############################################
-# execute with bash anchor_check.sh
-# To show only entries with sat/vb below N, execute with bash anchor_check.sh --limit N
-# For Umbrelians ☂️, execute with bash anchor_check.sh umbrel
-# To show only entries for a specific PUBKEY, execute with bash anchor_check.sh --peer PUBKEY
-
-#### One off #############################################
-# get a list of commitment fee for your peer list with this: 
-# $ lncli listchannels| jq -r '.channels | sort_by(.fee_per_kw | tonumber)[] | "\(.fee_per_kw | tonumber * 4 / 1000) sat/vb for \(.peer_alias) (\(.remote_pubkey))"' 
-# Generally a good advice to check the commitment type of your existing channels. With LND, you can get a list with
-# $ lncli listchannels | grep commitment_type
-# Caveat: For Umbrel ☂️, you need to have lncli as an alias: 
-# $ echo "alias lncli=\"/home/umbrel/umbrel/scripts/app compose lightning exec lnd lncli\"" >> ~/.bash_aliases
-# $ source ~/.bash_aliases
-
 # Help text
 help_text="
 NAME:
@@ -43,11 +24,10 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
     exit 0
 fi
 
+# Set the anchor_list variable based on --umbrel option
 if [[ $# -eq 1 && $1 == "--umbrel" ]]; then
-    # Set the anchor_list variable for umbrel
     anchor_list="/home/umbrel/umbrel/scripts/app compose lightning exec lnd lncli"
 else
-    # Set the anchor_list variable for any other system
     anchor_list="lncli"
 fi
 
@@ -55,55 +35,55 @@ fi
 if [[ $1 == "--peer" ]]; then
     PEER=$2
     LIMIT=$3
-    # Get nodeinfo for the specified pubkey
-    PEER_INFO=$($anchor_list getnodeinfo --pub_key "$PEER")
-else
-    LIMIT=$2
-fi
 
-# Define the list of channels sorted by total fee_per_kw ascending
-CHANNELS=$($anchor_list listchannels | jq '.channels | sort_by(.fee_per_kw | tonumber)')
+    # Fetch channel information for the specified pubkey
+    CHANNEL_INFO=$($anchor_list listchannels --peer "$PEER" | jq -r '.channels[0]')
 
-# Loop through each channel and extract the pubkey and fee_per_kw values
-while read -r line; do
-    # Extract the fee_per_kw and remote_pubkey values
-    FEE_PER_KW=$(echo "$line" | jq -r '.fee_per_kw')
-    PUBKEY=$(echo "$line" | jq -r '.remote_pubkey')
-
-    # Check if --peer is provided and skip if PUBKEY does not match
-    if [[ -n "$PEER" && "$PEER" != "$PUBKEY" ]]; then
-        continue
+    # Check if channel information is available
+    if [ -z "$CHANNEL_INFO" ]; then
+        echo "Invalid node pubkey or no channels found: $PEER"
+        exit 1
     fi
 
-    # Check if the pubkey is valid and get the alias name
-    ALIAS=$(echo "$line" | jq -r '.peer_alias')
-    if [ -z "$ALIAS" ]; then
-        # Use the pre-fetched info if available
-        if [ -n "$PEER_INFO" ]; then
-            ALIAS=$(echo "$PEER_INFO" | jq -r '.node.alias')
-        else
-            # Fetch alias from getnodeinfo if not available
-            ALIAS=$($anchor_list getnodeinfo --pub_key "$PUBKEY" | jq -r '.node.alias')
-            if [ -z "$ALIAS" ]; then
-                echo "Invalid node pubkey: $PUBKEY"
-                exit 1
-            fi
-        fi
-    fi
+    # Extract required information from the channel info
+    ALIAS=$(echo "$CHANNEL_INFO" | jq -r '.peer_alias')
+    FEE_PER_KW=$(echo "$CHANNEL_INFO" | jq -r '.fee_per_kw')
+    PUBKEY=$PEER
 
     # Calculate the sat_vbyte value
     SAT_VBYTE=$((FEE_PER_KW * 4 / 1000))
 
-    if [[ -z "$LIMIT" ]]; then
-        # LIMIT is empty, print everything
-        echo "$SAT_VBYTE sat/vb for $ALIAS ($PUBKEY)"
-    else
-        # LIMIT is not empty, proceed with filtering
-        if [ "$SAT_VBYTE" -lt "$LIMIT" ]; then
-            # Print everything below filter limit
+    # Print SAT_VBYTE value
+    echo "$SAT_VBYTE sat/vb for $ALIAS ($PUBKEY)"
+
+else
+    # If --limit is provided, set the limit
+    LIMIT=$2
+
+    # Fetch and parse channel information
+    CHANNELS=$($anchor_list listchannels | jq -c '.channels | sort_by(.fee_per_kw | tonumber)[]')
+
+    # Loop through each channel and extract the pubkey and fee_per_kw values
+    echo "$CHANNELS" | while read -r line; do
+        # Extract the fee_per_kw, remote_pubkey, and peer_alias values
+        FEE_PER_KW=$(echo "$line" | jq -r '.fee_per_kw')
+        PUBKEY=$(echo "$line" | jq -r '.remote_pubkey')
+        ALIAS=$(echo "$line" | jq -r '.peer_alias')
+
+        # Calculate the sat_vbyte value
+        SAT_VBYTE=$((FEE_PER_KW * 4 / 1000))
+
+        if [[ -z "$LIMIT" ]]; then
+            # LIMIT is empty, print everything
             echo "$SAT_VBYTE sat/vb for $ALIAS ($PUBKEY)"
         else
-            break
+            # LIMIT is not empty, proceed with filtering
+            if [ "$SAT_VBYTE" -lt "$LIMIT" ]; then
+                # Print everything below filter limit
+                echo "$SAT_VBYTE sat/vb for $ALIAS ($PUBKEY)"
+            else
+                break
+            fi
         fi
-    fi
-done <<< "$(echo "$CHANNELS" | jq -c '.[]')"
+    done
+fi
